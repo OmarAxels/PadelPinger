e7e7165fde4b422857f32d50a7ca751d2f969f28
OmarAxels <105921785+OmarAxels@users.noreply.github.com>
2025-08-17 16:03:43 +0000

    Update pinger.py

--- pinger.py
+++ pinger.py
@@ -2,6 +2,7 @@ import requests
 import datetime
 
 URL = "https://padel-scraper.onrender.com/"
+URL2 = "https://padelvaktin.is/"
 
 try:
     print(f"[{datetime.datetime.now()}] Pinging {URL}")
@@ -9,3 +10,11 @@ try:
     print(f"[{datetime.datetime.now()}] Pinged {URL} - Status: {response.status_code}")
 except Exception as e:
     print(f"[{datetime.datetime.now()}] Error: {e}")
+
+try:
+    print(f"[{datetime.datetime.now()}] Pinging {URL2}")
+    response = requests.get(URL2)
+    print(f"[{datetime.datetime.now()}] Pinged {URL2} - Status: {response.status_code}")
+except Exception as e:
+    print(f"[{datetime.datetime.now()}] Error: {e}")
+
